e3473d3ad1f2162b10e84af31bfa0301e764467b
Andreas Kluth <mail@andreaskluth.net>
2018-03-03 00:07:15 +0100

    Add metric.

--- micrometer-sample/src/main/java/net/andreaskluth/micrometersample/SampleController.java
+++ micrometer-sample/src/main/java/net/andreaskluth/micrometersample/SampleController.java
@@ -3,14 +3,21 @@ package net.andreaskluth.micrometersample;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class SampleController {
 
+    private final Counter counter = Metrics.counter("handler.calls", "uri", "/");
+
     @GetMapping("/")
     public Mono<String> hello() {
-        return Mono.just("Hallo");
+        return Mono.just("Hallo").map(__ -> {
+            counter.increment();
+            return __;
+        });
     }
 
 }
